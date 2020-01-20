ca1c2eb760400dcd8b8a618bb349c5dbba4ab5be
usuario <usuario@ubuntu-18.04-desktop-lts-amd64>
2020-01-20 20:23:41 +0100

    h

--- whatsapp.php
+++ whatsapp.php
@@ -1,11 +1,11 @@
 <?php
 $data = [
-    'phone' => '15034365851', // Receivers phone
-    'body' => 'Hello, world!', // Message
+    'phone' => '34688866966', // Receivers phone
+    'body' => 'HEEEEYYYYYYYYY', // Message
 ];
 $json = json_encode($data); // Encode data to JSON
 // URL for request POST /message
-$url = 'https://app.chat-api.com/instance/93398';
+$url = 'https://eu92.chat-api.com/instance93398/sendMessage?token=dp6kk14irlsk4b4i';
 // Make a POST request
 $options = stream_context_create(['http' => [
         'method'  => 'POST',
