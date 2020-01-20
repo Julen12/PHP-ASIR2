0e897382a4777f36f5319aec481f6daaf4ff131c
usuario <usuario@ubuntu-18.04-desktop-lts-amd64>
2020-01-20 19:12:41 +0100

    h

--- whatsapp.php
+++ whatsapp.php
@@ -1,45 +1,17 @@
 <?php
-if(!isset($_GET['text']) or !isset($_GET['phone'])){ die('Not enough data');}
-
-$apiURL = ' https://eu92.chat-api.com/instance93398/';
-$token = 'dp6kk14irlsk4b4i';
-
-$message = $_GET['text'];
-$phone = $_GET['phone'];
-
-$data = json_encode(
-    array(
-        'chatId'=>$phone.'@c.us',
-        'body'=>$message
-    )
-);
-$url = $apiURL.'message?token='.$token;
-$options = stream_context_create(
-    array('http' =>
-        array(
-            'method'  => 'POST',
-            'header'  => 'Content-type: application/json',
-            'content' => $data
-        )
-    )
-);
-$response = file_get_contents($url,false,$options);
-echo $response; exit;
-?>
-
-<html>
-
-
-<form action="whatsapp.php" method="POST">
-    <input type="text" name="message">
-    <br>
-    Password:
-    <br>
-
-    <input type="text" name="phone">
-    <br> <br>
-    <input type="submit" value="Submit">
-
-</form>
-</html> 
-
+$data = [
+    'phone' => '34688866966', // Receivers phone
+    'body' => 'Hello, iban!', // Message
+];
+$json = json_encode($data); // Encode data to JSON
+// URL for request POST /message
+$url = 'https://api.chat-api.com/message?token=dp6kk14irlsk4b4i';
+// Make a POST request
+$options = stream_context_create(['http' => [
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/json',
+        'content' => $json
+    ]
+]);
+// Send a request
+$result = file_get_contents($url, false, $options);
