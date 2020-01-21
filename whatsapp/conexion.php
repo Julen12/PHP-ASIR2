fdb74556a3883b279d7d8e5d756425a0769300a6
John Doe <johndoe@example.com>
2020-01-21 03:59:31 -0800

    h

--- whatsapp/conexion.php
+++ whatsapp/conexion.php
@@ -2,9 +2,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "Admin108";
-
+$port = "3307";
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $port);
 
 // Check connection
 if ($conn->connect_error) {
