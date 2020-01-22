1a09f99fddf4709cfd45ea4bf95f716b5d268b6d
AS3-2-15 <AS3-2-15@uni.lan>
2020-01-22 13:59:04 +0100

    h

--- whatsapp/whatsapp.php
+++ whatsapp/whatsapp.php
@@ -32,22 +32,27 @@ if(!isset($_SESSION["username"]))
 </body>
 </html>
 <?php
-$data = [
-    'phone' => $_POST['phone'], // Receivers phone
-    'body' => $_POST['body'], // Message
-];
-$json = json_encode($data); // Encode data to JSON
-// URL for request POST /message
-$url = 'https://eu92.chat-api.com/instance93398/sendMessage?token=dp6kk14irlsk4b4i';
-// Make a POST request
-$options = stream_context_create(['http' => [
-        'method'  => 'POST',
-        'header'  => 'Content-type: application/json',
-        'content' => $json
-    ]
-]);
-// Send a request
-$result = file_get_contents($url, false, $options);
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    $data = [
+        'phone' => $_POST['phone'], // Receivers phone
+        'body' => $_POST['body'], // Message   
+    ];
+    $json = json_encode($data); // Encode data to JSON
+    // URL for request POST /message
+    $url = 'https://eu92.chat-api.com/instance93398/sendMessage?token=dp6kk14irlsk4b4i';
+    // Make a POST request
+    $options = stream_context_create(['http' => [
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/json',
+            'content' => $json
+        ]
+    ]);
+    // Send a request
+    $result = file_get_contents($url, false, $options);
+    
+
+    
+
+
+
 
 
