ed188532dce8d6a983ab4e1fd30246ab780335d1
AS3-2-15 <AS3-2-15@uni.lan>
2020-01-23 10:27:18 +0100

    h

--- whatsapp/whatsapp.sql
+++ whatsapp/whatsapp.sql
@@ -1,7 +1,7 @@
 create database login;
 CREATE TABLE IF NOT EXISTS users (
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user VARCHAR(50) ,
+    user VARCHAR(50),
     pass VARCHAR(250)
 );
  CREATE TABLE telefono (numero VARCHAR(50));
